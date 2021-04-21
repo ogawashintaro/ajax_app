26c5b4234e1c3deb7d577d4a398229128a27913c
ogawashintaro <ogawas11234649@icloud.com>
2021-04-21 11:03:00 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,16 @@ class PostsController < ApplicationController
   def create
     Post.create(content: params[:content])
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(patams[:id])
+    render json: { post: item }
+  end
 end
